4d4a7af1aaf65d70133838f2570c37a8c649ce08
Sadioho <hoxuananh9.3@gmail.com>
2021-06-04 11:26:39 +0700

    Figo Restaurant

---
+++ main.js
@@ -0,0 +1,22 @@
+// LANDING PAGE ANIMATION
+var scene = document.getElementById("scene");
+var parallaxInstance1 = new Parallax(scene);
+
+// LANDING PAGE ANIMATION
+var scene2 = document.getElementById("scene-2");
+var parallaxInstances2 = new Parallax(scene2);
+
+var scene3 = document.getElementById("scene-3");
+var parallaxInstances3 = new Parallax(scene3);
+
+var scene4 = document.getElementById("scene-4");
+var parallaxInstances4 = new Parallax(scene4);
+
+var scene5 = document.getElementById("scene-5");
+var parallaxInstances5 = new Parallax(scene5);
+
+// var scene6 = document.getElementById("scene-6");
+// var parallaxInstances6 = new Parallax(scene6);
+
+// var scene7 = document.getElementById("scene-7");
+// var parallaxInstances7 = new Parallax(scene7);
